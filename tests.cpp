eb2b0a2839a4a1b8193f6339bcbccae1e22be54d
MarcusVinix <vi.ni159marcus@gmail.com>
2022-05-12 08:41:10 -0300

    Preparando ambiente para testes

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm.hpp>
+#include <utility.hpp>
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace std;
+
+TEST(Vector, test) {
+	string name = "Marcus";
+	EXPECT_EQ(name, "Marcus");
+}
